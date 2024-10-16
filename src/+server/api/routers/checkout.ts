a7d8fbcf6408f8157c87bfe699fb315856a0a28d
Leon Meka <leonmeka@gmail.com>
2024-10-16 15:22:31 +0200

    chore: change url

--- src/+server/api/routers/checkout.ts
+++ src/+server/api/routers/checkout.ts
@@ -4,7 +4,7 @@ export const checkoutRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const email = ctx.session?.user?.email;
 
-    const url = `https://store.stable-pixel.com?email=${email}`;
+    const url = `https://shop.stable-pixel.com?email=${email}`;
 
     return {
       url,
