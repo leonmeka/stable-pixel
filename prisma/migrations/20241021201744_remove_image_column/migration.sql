9f48e41823b9eb10c5857c1b8c1958764ba5121b
Leon Meka <leonmeka@gmail.com>
2024-10-21 22:17:57 +0200

    chore: remove image column

---
+++ prisma/migrations/20241021201744_remove_image_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `image` on the `Prediction` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Prediction" DROP COLUMN "image";
